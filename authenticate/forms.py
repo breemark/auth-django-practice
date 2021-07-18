622e40d3b3717fc3d6ae95e1b22ae17ce4450d77
torres <britomaximiliano83@gmail.com>
2021-07-19 04:15:37 +0700

    widgets to style the register form

--- authenticate/forms.py
+++ authenticate/forms.py
@@ -4,18 +4,25 @@ from django import forms
 from .models import Profile
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
 
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+
 
 # Favorite Color form
 class FavColorForm(forms.ModelForm):
-    fav_color = forms.CharField(max_length=100)
+    fav_color = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = Profile
